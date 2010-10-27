46dc0599cdf6843a48fff5b83cad0cae9bb31113
Lukas Rytz <lukas.rytz@epfl.ch>
2010-10-27 10:57:30 +0200

    type-correct handling of Set.empty

--- examples/src/effects/colls/St.scala
+++ examples/src/effects/colls/St.scala
@@ -6,7 +6,7 @@ trait Addable[A, +Repr <: Addable[A, Repr]] { self: Repr =>
 
 trait StLk[A, +This <: StLk[A, This] with St[A]] extends ItrblLk[A, This] with Addable[A, This] { self: This =>
   def empty: This
-  override protected[this] def newBuilder: Bldr[A, This] = new AddBldr[A, This](empty.asInstanceOf[This]) // @TODO: why cast??? bug?
+  override protected[this] def newBuilder: Bldr[A, This] = new AddBldr[A, This](empty)
   def contains(elem: A): Boolean
   def + (elem: A): This
   def - (elem: A): This
@@ -27,11 +27,7 @@ abstract class StFct[CC[X] <: St[X] with StLk[X, CC[X]]] extends GenCpn[CC] {
   }
 }
 
-trait GenStTmpl[A, +CC[X] <: St[X]] extends GenTravTmpl[A, CC] {
-  def empty: CC[A] = companion.empty[A]
-}
-
-trait St[A] extends (A => Boolean) with Itrbl[A] with GenStTmpl[A, St] with StLk[A, St[A]] {
+trait St[A] extends (A => Boolean) with Itrbl[A] with GenTravTmpl[A, St] with StLk[A, St[A]] {
   override def companion: GenCpn[St] = St
 }
 
@@ -41,8 +37,23 @@ object St extends StFct[St] {
 }
 
 
-class HSt[A](private val els: collection.immutable.Set[A] = new collection.immutable.HashSet[A]()) extends St[A] with GenStTmpl[A, HSt] with StLk[A, HSt[A]] { self =>
+class HSt[A](private val els: collection.immutable.Set[A] = new collection.immutable.HashSet[A]()) extends St[A] with GenTravTmpl[A, HSt] with StLk[A, HSt[A]] { self =>
   override def companion: GenCpn[HSt] = HSt
+
+  /**
+   * The real collections define GenStTmpl, and provide an implementation for
+   * "def empty" there. However, this doesn't allow StLk to type check. With a
+   * concrete method in GenStTmpl
+   *   def empty: CC[A] = companion.empty[A]
+   *
+   * The concrete method will override the abstract one in StLk
+   *   def empty: This
+   *
+   * and the type of "empty" in StLk is CC[A], i.e. Set[A], not This. So
+   * the constructor call "new AddBldr[A, This](empty)" fails, because it
+   * expects a This, but gets a Set[A].
+   */
+  def empty: HSt[A] = companion.empty[A]
 
   def size = els.size
   def contains(elem: A): Boolean = els(elem)
